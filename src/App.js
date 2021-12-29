0f736ee03671d39b55ea35357e4a4ab4a14602fd
rokwol <roksolanawoloszyn@gmail.com>
2021-12-29 00:40:43 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App</h1>
+      <footer>
+        This project was coded by Roksolana Woloszyn and is open-sourced{" "}
+        <a
+          href="https://github.com/rokwol/weather-search-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on Github.
+        </a>
+      </footer>
     </div>
   );
 }
