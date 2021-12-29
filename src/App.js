520f5b7c09240ef2ba1b564572486bfe90f471a1
rokwol <roksolanawoloszyn@gmail.com>
2021-12-29 17:13:22 -0500

    Imported bootstrap and added Weather component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Roksolana Woloszyn and is open-sourced{" "}
-        <a
-          href="https://github.com/rokwol/weather-search-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Roksolana Woloszyn and is open-sourced{" "}
+          <a
+            href="https://github.com/rokwol/weather-search-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
