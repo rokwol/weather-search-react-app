1df399be0a546501b8a5fe971d7359aa217991a6
rokwol <roksolanawoloszyn@gmail.com>
2022-01-02 23:13:46 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Vancouver" />
         <small>
           This project was coded by Roksolana Woloszyn and is open-sourced{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="Submit"
-              value="Search"
-              className="btn btn-outline-info w-100"
-              autoComplete="off"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Toronto, Canada</h1>
-      <ul>
-        <li> Wednesday, December 29, 2021 </li>
-        <li> Last updated: 21:51:08 </li>
-        <li> Mist</li>
-      </ul>
+export default function Weather(props) {
+  const [weather, setWeather] = useState({ loaded: false });
 
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix">
-            <span className="float-left">
-              <img
-                src="http://openweathermap.org/img/wn/50n@2x.png"
-                alt="Mist icon"
+  function handleResponse(response) {
+    setWeather({
+      loaded: true,
+      date: "Sunday, January 2, 2022",
+      city: response.data.name,
+      country: response.data.sys.country,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      pressure: response.data.main.pressure,
+      description: response.data.weather[0].description,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  if (weather.loaded) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
               />
-            </span>
-            <span className="float-left">
-              <span className="temperature">4</span>
-              <span className="unit">°C </span>
-            </span>
+            </div>
+            <div className="col-3">
+              <input
+                type="Submit"
+                value="Search"
+                className="btn btn-outline-info w-100"
+                autoComplete="off"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-6 mt-2">
-          <ul>
-            <li> Wind: 4 km/h</li>
-            <li> Humidity: 95%</li>
-            <li> Pressure: 1012 hPA</li>
-          </ul>
+        </form>
+        <h1>
+          {weather.city}, {weather.country}
+        </h1>
+        <ul>
+          <li> {weather.date} </li>
+          <li> Last updated: 21:51:08 </li>
+          <li className="text-capitalize"> {weather.description}</li>
+        </ul>
+
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix">
+              <span className="float-left">
+                <img src={weather.iconUrl} alt={weather.description} />
+              </span>
+              <span className="float-left">
+                <span className="temperature">
+                  {Math.round(weather.temperature)}
+                </span>
+                <span className="unit">°C </span>
+              </span>
+            </div>
+          </div>
+          <div className="col-6 mt-2">
+            <ul>
+              <li> Wind: {Math.round(weather.wind)} km/h</li>
+              <li> Humidity: {weather.humidity}%</li>
+              <li> Pressure: {weather.pressure} hPA</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "a80063ef5edd7b93d32d44b227457f4e";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading ...";
+  }
 }
