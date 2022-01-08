068cef382548c01c6ba06e937a43d65236c9da9e
rokwol <roksolanawoloszyn@gmail.com>
2022-01-08 15:08:42 -0500

    Improved api url

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather(props) {
 
   function searchLocation() {
     const apiKey = "a80063ef5edd7b93d32d44b227457f4e";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
